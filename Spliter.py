93b86ff83704ec31bccf59714c5bb92762d94967
Ahmed Hassan <ahmed4321medohasan@gmail.com>
2023-11-27 18:39:34 +0200

    Some Modifications in Parsers

--- Spliter.py
+++ Spliter.py
@@ -8,26 +8,38 @@ parser.add_argument(
     "-i", "--Video_Input", type=str, help="Video will be splited", required=True
 )
 parser.add_argument(
-    "-dir", "--directory", type=str, help="your directory for output", required=False
+    "-dir",
+    "--directory",
+    type=str,
+    help="your directory for output",
+    required=False,
+    default=os.getcwd(),
 )
-parser.add_argument("-n", "-name", "--Name", type=str, help="file name", required=True)
+parser.add_argument(
+    "-n",
+    "-name",
+    "--Name",
+    type=str,
+    help="file name",
+    required=False,
+    default="output",
+)
 parser.add_argument(
     "-t", "-time", "--Time", type=int, help="time for each video", required=True
 )
 parser.add_argument(
-    "-e", "-extension", "--Extension", type=str, help="def: ts", required=False
+    "-e",
+    "-extension",
+    "--Extension",
+    type=str,
+    help="default : mp4",
+    required=False,
+    default="mp4",
 )
 args = parser.parse_args()
-if args.Extension == None:
-    ex = "ts"
-else:
-    ex = args.Extension
-if args.directory == None:
-    dir = os.getcwd()
-else:
-    dir = args.directory
+
 ffmpeg.input(args.Video_Input).output(
-    f"{dir}/{args.Name}_%03d.{ex}",
+    f"{dir}/{args.Name}_%03d.{args.Extension}",
     c="copy",
     f="segment",
     segment_time=args.Time,
